02c806c47b3f395c874688c6f88605ff1f70ac77
Jovica <jovica.petric@croonus.com>
2024-02-08 11:38:58 +0100

    /WindyGrill  - Localization of selected language

--- WindyGrill/App.js
+++ WindyGrill/App.js
@@ -1,7 +1,14 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import Router from './src/navigation/Router';
+import { checkIfLanguageSelected } from './src/redux/actions/LanguageActions';
 
 export const App = () => {
+  const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    dispatch(checkIfLanguageSelected());
+  }, []);
   return (
       <Router />
   );
